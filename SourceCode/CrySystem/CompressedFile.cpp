11c89b143aeb6cf9f9a92fee3686d94377fdc4f0
Aleksey Komarov <q4arus@ya.ru>
2023-12-02 12:41:51 +0300

    CrySystem: fix double file close in CompressedFile.cpp
    based on:
    https://github.com/n1f7/FarCry/commit/6ed6cbda03183d15b2268b3f0a76fa6911032bce
    https://www.zlib.net/manual.html

--- SourceCode/CrySystem/CompressedFile.cpp
+++ SourceCode/CrySystem/CompressedFile.cpp
@@ -62,7 +62,6 @@ unsigned int CSystem::GetCompressedFileSize(char *filename)
 	gzFile f = gzdopen(fileno(pFile), "rb9");
 	gzread(f, &bitlen, sizeof(int));
 	gzclose(f);
-	fclose(pFile);
 #else
 	fseek(pFile, 0, SEEK_END);
 	long nLen = ftell(pFile);
@@ -102,7 +101,6 @@ unsigned int CSystem::ReadCompressedFile(char *filename, void *data, unsigned in
 	assert(bitlen<=maxbitlen);  // FIXME: nicer if caller doesn't need to know buffer size in advance
 	gzread(f, data, BITS2BYTES(bitlen));
 	gzclose(f);
-	fclose(pFile);
 #else	
 	fread(&bitlen, sizeof(int), 1, pFile);
 	assert(bitlen<=maxbitlen);  // FIXME: nicer if caller doesn't need to know buffer size in advance
